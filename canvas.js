d96b4d0ffbb2e94f3c5aa5bd715623bfd90bff9c
maheshtalluri <tallurimahesh169@gmail.com>
2022-12-14 17:53:36 +0530

    canvas

---
+++ canvas.js
@@ -0,0 +1,23 @@
+let circle1 = document.getElementById("circle1");
+let circle2 = document.getElementById("circle2");
+let circle3 = document.getElementById("circle3");
+let circle4 = document.getElementById("circle4");
+
+
+
+
+function onClickBtn1() {
+    circle1.classList.toggle("by-btn-click");
+}
+
+function onClickBtn2() {
+    circle2.classList.toggle("by-btn-click");
+}
+
+function onClickBtn3() {
+    circle3.classList.toggle("by-btn-click");
+}
+
+function onClickBtn4() {
+    circle4.classList.toggle("by-btn-click");
+}
